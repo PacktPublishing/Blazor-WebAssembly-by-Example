332cbd463add44d17c92004cc269c8da6d81ba07
beverita <toi@onestopdesigns.com>
2021-03-13 16:54:05 -0600

    Chapter 2

---
+++ ch02/Demo/Demo/Pages/Counter.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Pages
+{
+    public partial class Counter
+    {
+        [Parameter] public int? Increment { get; set; }
+        private int increment = 1;
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount += increment;
+        }
+
+        protected override void OnParametersSet()
+        {
+            if (Increment.HasValue)
+                increment = Increment.Value;
+        }
+    }
+}
