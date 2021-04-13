3df0addc63e65930540d431a58bab13b3aa6e498
beverita <toi@onestopdesigns.com>
2021-04-12 19:03:19 -0500

    Chapter 7

---
+++ ch07/KanbanBoard/Models/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KanbanBoard.Models
+{
+    public class TaskItem
+    {
+        public string TaskName { get; set; }
+        public TaskPriority Priority { get; set; }
+    }
+}
---
+++ ch07/KanbanBoard/Models/TaskPriority.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KanbanBoard.Models
+{
+    public enum TaskPriority
+    {
+        High,
+        Medium,
+        Low
+    }
+}
